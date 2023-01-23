760b2d0a1561627346774d72cfa1aa74009f29d5
unknown <jeefx123@gmail.com>
2023-01-23 15:41:59 +0400

    add

--- src/pages/components/UserGuide.jsx
+++ src/pages/components/UserGuide.jsx
@@ -6,13 +6,16 @@ function UserGuide() {
       <p className="text-2xl font-semibold max-md:text-lg max-sm:text-base">
         Dapp Guide:
       </p>
-      <ul>
+      <ul className="list-disc">
         <li>Token name field: Input the desired name of the Token</li>
         <li>Symobl field: Input the desiered symbol of the Token</li>
         <li>
           Total supply field: Input the desiered total supply of the Token
         </li>
-        <li></li>
+        <li>
+          After the deployment: open up the metamask extansion Metamask | Assets
+          | Import tokens | Insert address | Add custom token
+        </li>
       </ul>
     </div>
   );
